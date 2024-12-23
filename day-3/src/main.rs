1d7d256913d05b08c62b90d79016fd8cdda4f3d3
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-12-23 16:44:45 -0300

    day 3 completed

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -12,31 +12,44 @@ fn main() -> Result<(), Error> {
     println!("the sum is {p1}");
 
     let p2 = p2(file).unwrap();
+    println!("the sum is {p2}");
 
     Ok(())
 }
 
 fn p1(f: &str) -> Result<u32, Error> {
-    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
-    let mut nums: Vec<[u32; 2]> = Vec::new();
-
-    for (_, [a, b]) in re
-        .captures_iter(&fs::read_to_string(f)?)
-        .map(|c| c.extract())
-    {
-        nums.push([a.parse().unwrap(), b.parse().unwrap()]);
+    let re = Regex::new(r"mul\((?<num1>[0-9]{1,3}),(?<num2>([0-9]{1,3}))\)").unwrap();
+    let mut result: u32 = 0;
+    for caps in re.captures_iter(&fs::read_to_string(f)?) {
+        if let (Some(num1), Some(num2)) = (caps.name("num1"), caps.name("num2")) {
+            let num1 = num1.as_str().parse::<u32>().unwrap();
+            let num2 = num2.as_str().parse::<u32>().unwrap();
+            result += num1 * num2;
+        }
     }
-
-    let mut res: Vec<u32> = Vec::new();
-    for arr in nums {
-        res.push(arr[0] * arr[1]);
-    }
-    Ok(res.iter().sum::<u32>())
+    Ok(result)
 }
 
-fn p2(f: &str) -> Result<(), Error> {
-    
-
-
-    Ok(())
+fn p2(f: &str) -> Result<u32, Error> {
+    let re = Regex::new(
+        r"(?<cmd_do>do\(\))|(?<cmd_dont>don't\(\))|mul\((?<num1>[0-9]{1,3}),(?<num2>[0-9]{1,3})\)",
+    )
+    .unwrap();
+    let content = fs::read_to_string(f)?;
+    let mut enabled = true;
+    let mut result: u32 = 0;
+    for cap in re.captures_iter(&content) {
+        if cap.name("cmd_do").is_some() {
+            enabled = true;
+        } else if cap.name("cmd_dont").is_some() {
+            enabled = false;
+        } else if let (Some(num1), Some(num2)) = (cap.name("num1"), cap.name("num2")) {
+            if enabled {
+                let num1 = num1.as_str().parse::<u32>().unwrap();
+                let num2 = num2.as_str().parse::<u32>().unwrap();
+                result += num1 * num2;
+            }
+        }
+    }
+    Ok(result)
 }
